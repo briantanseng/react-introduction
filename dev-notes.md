3e12fbf04d1e90dc1e1262b1830b231c679f460f
briantanseng <brian.tanseng@98labs.com>
2016-08-01 22:42:13 -0700

    03. Added Moment.js

--- dev-notes.md
+++ dev-notes.md
@@ -6,3 +6,10 @@ npm install moment-timezone --save
 
 https://www.npmjs.com/package/timezones.json
 npm install timezones.json --save
+
+https://www.npmjs.com/package/ntp-client
+npm install ntp-client
+
+02. Add Moment.js
+
+http://momentjs.com/docs/
--- src/time/TimeComponent.js
+++ src/time/TimeComponent.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import './TimeComponent.css';
+import moment from 'moment';
 
 class TimeComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    // state is initialized
+    this.state = {
+      currentTime: moment().format()
+    }
+
+  }
+
+  componentDidMount() {
+    // modify the state
+    this.id = setInterval(() => {
+      this.setState({ currentTime: moment().format() });
+    }, 1000);
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.id);
+  }
+
   render() {
     return (
-      <div className="TimeComponent-display">{new Date().toString()}</div>
+      <div className="TimeComponent-display">
+        Time: { this.state.currentTime }
+      </div>
     );
   }
 }
